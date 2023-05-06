f5166ce0e4b06abaf5003f03a0145b2350bec659
Serhat Gündoğdu <serhat.gundogdu@outlook.com>
2023-05-06 17:58:38 +0300

    Earthquake list added!

--- lib/screens/donor_page.dart
+++ lib/screens/donor_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:depremapp/components/earthquake_list_item.dart';
 import 'package:depremapp/components/request_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hex_color/flutter_hex_color.dart';
@@ -33,20 +34,66 @@ class _DonorPageState extends State<DonorPage> {
           ),
         ),
       ),
-      body: Container(
-          margin: EdgeInsets.only(top: 25),
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 16),
-                child: Wrap(
-                  runSpacing: 12,
-                  children: const [RequestContainer()],
+      body: SingleChildScrollView(
+        child: Container(
+            margin: EdgeInsets.only(top: 25),
+            padding: EdgeInsets.all(16),
+            child: Wrap(
+              runSpacing: 12,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 16),
+                  child: Wrap(
+                    runSpacing: 12,
+                    children: const [RequestContainer()],
+                  ),
                 ),
-              ),
-            ],
-          )),
+                Text(
+                  "Son Depremler",
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24,
+                    color: HexColor('222B45'),
+                  ),
+                ),
+                EarthQuakeItem(
+                  city: "İzmir",
+                  date: "13:26 Bugün",
+                  magnitude: 7.4,
+                ),
+                EarthQuakeItem(
+                  city: "Adana",
+                  date: "13:24 Bugün",
+                  magnitude: 1.4,
+                ),
+                EarthQuakeItem(
+                  city: "İstanbul",
+                  date: "13:26 Bugün",
+                  magnitude: 2.1,
+                ),
+                EarthQuakeItem(
+                  city: "Çanakkale",
+                  date: "13:26 Bugün",
+                  magnitude: 2.6,
+                ),
+                EarthQuakeItem(
+                  city: "İzmir",
+                  date: "13:26 Bugün",
+                  magnitude: 7.4,
+                ),
+                EarthQuakeItem(
+                  city: "İzmir",
+                  date: "13:26 Bugün",
+                  magnitude: 7.4,
+                ),
+                EarthQuakeItem(
+                  city: "İzmir",
+                  date: "13:26 Bugün",
+                  magnitude: 7.4,
+                )
+              ],
+            )),
+      ),
     );
   }
 }
