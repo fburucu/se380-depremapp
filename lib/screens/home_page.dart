3f1a94854ecb16a179aa1864d09960cf89ddf65e
fburucu <75666945+fburucu@users.noreply.github.com>
2023-05-17 03:29:53 +0300

    Updated home page
    Edited and updated the home page.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,6 +5,7 @@ import 'package:depremapp/screens/donor_page.dart';
 import 'package:depremapp/screens/victim_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hex_color/flutter_hex_color.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,23 +26,126 @@ class _HomePageState extends State<HomePage> {
           child: Center(
               child: Column(
             children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => VictimPage()),
-                  );
-                },
-                child: Text("Depremzede"),
+              Text(
+                'Giriş Yap',
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 24,
+                  color: HexColor('222B45'),
+                ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => DonorPage()),
-                  );
-                },
-                child: Text("Bağışçı"),
+              SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 70,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => VictimPage()),
+                    );
+                  },
+                  child: Text(
+                      "Depremzede",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 70,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                    ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DonorPage()),
+                    );
+                  },
+                  child: Text(
+                      "Bağışçı",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Text(
+                      'Hızlı işlemler',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    constraints: BoxConstraints(minWidth: 200, maxWidth: 350),
+                    padding: EdgeInsets.all(30),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Colors.white,
+                    ),
+
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              'İhtiyaçlarını Gönder',
+                            ),
+                            Icon(
+                              Icons.arrow_forward_rounded, 
+                              size: 30,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              'İhtiyaç Listesini Göster',
+                            ),
+                            Icon(
+                            Icons.arrow_forward_rounded,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              'Payment link'
+                            ),
+                            Icon(
+                              Icons.arrow_forward_rounded,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               )
             ],
           ))),
