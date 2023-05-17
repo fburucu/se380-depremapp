42ce3c58b257a52efb73c0ebc0668a7c38f9484d
fburucu <75666945+fburucu@users.noreply.github.com>
2023-05-17 16:56:06 +0300

    Updated new_request_page
    Added singlechildscrollview to column.

--- lib/screens/new_request_page.dart
+++ lib/screens/new_request_page.dart
@@ -21,175 +21,177 @@ class _NewRequestState extends State<NewRequest> {
       body: Container(
         margin: EdgeInsets.only(top:25),
         padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Yeni Talep",
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 24,
+                        color: HexColor('222B45'),
+                      ),
+                    ),
+                  ],
+              ),
+              Row(
                 children: [
+                  SizedBox(
+                    height: 70,
+                  ),
+                  Text("Ad Soyad",
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14,
+                    color: HexColor('222B45'),
+                  ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 350,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adınız Soyadınız',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    height: 70,
+                  ),
                   Text(
-                    "Yeni Talep",
+                    'Telefon Numarası',
                     style: GoogleFonts.poppins(
                       fontWeight: FontWeight.w500,
-                      fontSize: 24,
+                      fontSize: 14,
                       color: HexColor('222B45'),
                     ),
                   ),
                 ],
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  height: 70,
-                ),
-                Text("Ad Soyad",
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 14,
-                  color: HexColor('222B45'),
-                ),
-                )
-              ],
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 350,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Adınız Soyadınız',
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 350,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Telefon Numaranız',
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  height: 70,
-                ),
-                Text(
-                  'Telefon Numarası',
-                  style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 14,
-                    color: HexColor('222B45'),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    height: 70,
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 350,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Telefon Numaranız',
+                  Text(
+                      'Adres',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      color: HexColor('222B45'),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  height: 70,
-                ),
-                Text(
-                    'Adres',
-                  style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 14,
-                    color: HexColor('222B45'),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 350,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adresiniz',
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    height: 70,
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 350,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Adresiniz',
+                  Text(
+                    'İhtiyaçlarınız',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      color: HexColor('222B45'),
                     ),
                   ),
-                )
-              ],
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  height: 70,
-                ),
-                Text(
-                  'İhtiyaçlarınız',
-                  style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 14,
-                    color: HexColor('222B45'),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 350,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'İhtiyaçlarınız..',
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 350,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'İhtiyaçlarınız..',
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                SizedBox(
-                  height: 70,
-                ),
-                SizedBox(
-                  width: 100,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.pinkAccent,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                       context,
-                        MaterialPageRoute(builder: (context) => VictimPage()),
-                      );
-                    },
-                    child: Text("İptal"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    height: 70,
                   ),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                SizedBox(
-                  width: 100,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black38,
+                  SizedBox(
+                    width: 100,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pinkAccent,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                         context,
+                          MaterialPageRoute(builder: (context) => VictimPage()),
+                        );
+                      },
+                      child: Text("İptal"),
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => VictimPage()),
-                      );
-                    },
-                    child: Text("Kayıt Et"),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: 20,
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black38,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => VictimPage()),
+                        );
+                      },
+                      child: Text("Kayıt Et"),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
