e1630362686a24902eddb8109dec974b77afd33d
Serhat Gündoğdu <serhat.gundogdu@outlook.com>
2023-05-24 15:53:30 +0300

    Firebase integration to the app

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,14 @@ import 'package:depremapp/screens/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
