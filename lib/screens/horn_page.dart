765bf0eab73a2f6f7eaa3bdfb7b27e7a1a247664
Serhat Gündoğdu <serhat.gundogdu@outlook.com>
2023-06-05 03:01:13 +0300

    Whistle Page

---
+++ lib/screens/horn_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hex_color/flutter_hex_color.dart';
+
+import '../components/static/header_component.dart';
+
+class HornPage extends StatelessWidget {
+  const HornPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: HexColor("F2F1F6"),
+        appBar: Header(),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(20),
+                  primary: Colors.red,
+                  shape: CircleBorder()),
+              child: const Icon(
+                Icons.sports,
+                size: 52,
+              )),
+        ));
+  }
+}
