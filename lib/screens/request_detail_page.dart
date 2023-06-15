fa8a60679ae24f1ad713cb2dbd668fb4fc67c11b
fburucu <75666945+fburucu@users.noreply.github.com>
2023-06-15 22:44:33 +0300

    Detail Map location fixed

--- lib/screens/request_detail_page.dart
+++ lib/screens/request_detail_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hex_color/flutter_hex_color.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-
+import 'package:geocoding/geocoding.dart';
 
 import '../components/static/header_component.dart';
 
@@ -18,13 +18,48 @@ class RequestDetail extends StatefulWidget {
 }
 
 class _RequestDetailState extends State<RequestDetail> {
-  // void initState() {
+  bool loading=true;
+  bool error=false;
+  double latitude=0;
+   double longitude=0;
+  @override
+  void initState() {
+    convertAdressToLatLong();
+    super.initState();
+  }
 
-  //   super.initState();
-  // }
+  void convertAdressToLatLong() async {
+    try {
+      List<Location> locations = await locationFromAddress(
+          widget.request['address']);
+
+      if (locations.isNotEmpty) {
+        setState(() {
+          latitude = locations.first.latitude;
+          longitude = locations.first.longitude;
+          loading = false;
+        });
+      }
+    }
+    catch(e){
+      setState(() {
+        error=true;
+        loading=false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (loading) {
+      return Scaffold(
+        backgroundColor: HexColor("F2F1F6"),
+        appBar: Header(),
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
     return Scaffold(
         backgroundColor: HexColor("F2F1F6"),
         appBar: Header(),
@@ -57,13 +92,27 @@ class _RequestDetailState extends State<RequestDetail> {
               SizedBox(
                 height: 200,
                 child: FlutterMap(
-                  options: MapOptions(center: LatLng(51.5, -0.09), zoom: 13.0),
+                  options: MapOptions(center: LatLng(latitude,longitude), zoom: 13.0),
                   children: [
                     TileLayer(
                       urlTemplate:
                           'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                       userAgentPackageName: 'com.example.app',
                     ),
+                    MarkerLayer(
+                      markers: [
+                        Marker(
+                          point:
+                          LatLng(latitude, longitude),
+                          builder: (BuildContext context) {
+                            return const Icon(
+                              Icons.location_on,
+                              color: Colors.red,
+                            );
+                          },
+                        )
+                      ],
+                    )
                   ],
                 ),
               ),
@@ -77,6 +126,8 @@ class _RequestDetailState extends State<RequestDetail> {
                         style: TextStyle(
                             fontWeight: FontWeight.w600, color: Colors.black54),
                       ),
+                      if(error)
+                        Text('Girilen adres konumlandırılamadı', style: TextStyle(color: Colors.red),),
                       Divider(),
                       Text(
                         "İhtiyaçlar",
