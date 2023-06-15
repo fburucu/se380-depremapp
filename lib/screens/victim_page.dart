82d87d83da7cecc6ece21eebe02ebd2464e6f261
Serhat Gündoğdu <serhat.gundogdu@outlook.com>
2023-06-16 02:38:17 +0300

    Show User's own Taleps

--- lib/screens/victim_page.dart
+++ lib/screens/victim_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:depremapp/components/static/header_component.dart';
 import 'package:depremapp/screens/new_request_page.dart';
 import 'package:depremapp/screens/phone_number_page.dart';
@@ -19,6 +20,9 @@ class VictimPage extends StatefulWidget {
 }
 
 class _VictimPageState extends State<VictimPage> {
+  bool isLoaded = false;
+  List<Map> taleps = [];
+
   String phone = "";
 
   late SharedPreferences prefs;
@@ -31,7 +35,22 @@ class _VictimPageState extends State<VictimPage> {
     super.initState();
   }
 
-  void chechPhoneEntered() async {
+  Future<void> getTaleps() async {
+    setState(() {
+      isLoaded = false;
+    });
+    final query = FirebaseFirestore.instance.collection('victims');
+
+    final qSnap = await query.where('phone', isEqualTo: phone).get();
+
+    setState(() {
+      taleps = qSnap.docs.map((e) => e.data()).toList();
+      isLoaded = true;
+    });
+    print(taleps);
+  }
+
+  Future<void> chechPhoneEntered() async {
     // try {
     SharedPreferences.setMockInitialValues({});
 
@@ -55,64 +74,86 @@ class _VictimPageState extends State<VictimPage> {
       setState(() {
         phone = phoneNumber;
       });
+
+      await getTaleps();
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    if (!isLoaded) {
+      return Scaffold(
+        backgroundColor: HexColor("F2F1F6"),
+        appBar: Header(),
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: HexColor("F2F1F6"),
       appBar: Header(),
-      body: Container(
-          margin: EdgeInsets.only(top: 25),
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await getTaleps();
+        },
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Container(
+              margin: EdgeInsets.only(top: 25),
+              padding: EdgeInsets.all(16),
+              child: Column(
                 children: [
-                  Text(
-                    "Taleplerin",
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 24,
-                      color: HexColor('222B45'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Taleplerin",
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 24,
+                          color: HexColor('222B45'),
+                        ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        )),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => NewRequest()),
+                          );
+                        },
+                        child: Text("Yeni Talep Ekle",
+                            style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: HexColor('222B45'),
+                            )),
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 16),
+                    child: Wrap(
+                      runSpacing: 12,
+                      children: taleps.map((e) => Talep(e)).toList(),
                     ),
                   ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    )),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => NewRequest()),
-                      );
-                    },
-                    child: Text("Yeni Talep Ekle",
-                        style: GoogleFonts.poppins(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16,
-                          color: HexColor('222B45'),
-                        )),
-                  )
+                  if (taleps.isEmpty)
+                    const Text(
+                      "Talep bulunmamaktadır.",
+                      style: TextStyle(color: Colors.red, fontSize: 16),
+                    )
                 ],
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 16),
-                child: Wrap(
-                  runSpacing: 12,
-                  children: const [
-                    // Talep(),
-                    // Talep(),
-                    // Talep(),
-                  ],
-                ),
-              ),
-            ],
-          )),
+              )),
+        ),
+      ),
     );
   }
 }
